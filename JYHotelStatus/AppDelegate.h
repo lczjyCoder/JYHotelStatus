897807e15b5660a6d4f7b181ffd71086eaf36be9
zhangjiuyang <zhangjiuyang@lvmama.com>
2017-03-31 17:51:35 +0800

    Initial Commit

---
+++ JYHotelStatus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYHotelStatus
+//
+//  Created by zjy on 2017/3/31.
+//  Copyright © 2017年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYHotelStatus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYHotelStatus
+//
+//  Created by zjy on 2017/3/31.
+//  Copyright © 2017年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
